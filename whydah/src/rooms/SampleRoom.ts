6f33cc3f6a141075dfb9baa61f39d13982e38a94
kwojciechowski <hgielo@gmail.com>
2021-05-05 21:38:24 +0200

    feat: sample room

---
+++ whydah/src/rooms/SampleRoom.ts
@@ -0,0 +1,21 @@
+import { Client, Room } from "colyseus";
+import SampleState from "../state/SampleState";
+
+export default class SampleRoom extends Room<SampleState> {
+	onCreate(options: unknown): void {
+		super.onCreate?.(options);
+		this.setState(new SampleState());
+	}
+
+	onJoin(client: Client, options?: unknown, auth?: unknown): void {
+		super.onJoin?.(client, options, auth);
+		console.log(client.sessionId, " joined");
+		this.state.createPlayer(client.sessionId);
+	}
+
+	onLeave(client: Client, consented?: boolean): void {
+		super.onLeave?.(client, consented);
+		console.log(client.sessionId, " left");
+		this.state.removePlayer(client.sessionId);
+	}
+}
