94168b39e42ad9859bc1ba9fe9701e4745a88921
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2021-08-24 21:15:39 +0200

    feat(multiplayer): random int fn and export player types

--- ambrose-light/index.ts
+++ ambrose-light/index.ts
@@ -1,1 +1,9 @@
 export const isDev = () => process.env.NODE_ENV === "development";
+
+export const getRandomInt = (a, b) => {
+  const min = Math.ceil(a);
+  const max = Math.floor(b);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export * from "./types/players";
