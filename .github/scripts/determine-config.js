e1992016765b3523f12c1723517cc0c0b2242f95
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2021-08-30 23:54:47 +0200

    chore(ci/cd): scripts

--- .github/scripts/determine-config.js
+++ .github/scripts/determine-config.js
@@ -1,4 +1,13 @@
-module.exports = ({ ...args }) => {
-  console.log(args);
-  return {};
+const MAIN_DOMAIN = "aye-bucko.pl";
+
+module.exports = ({ context }) => {
+  const [branchName] = context.split("/").reverse();
+
+  const frontendHost =
+    branchName === "master" ? `${branchName}.${MAIN_DOMAIN}` : MAIN_DOMAIN;
+  const backendHost =
+    branchName === "master" ? `${branchName}.${MAIN_DOMAIN}` : MAIN_DOMAIN;
+  const backendAddress = `ws://${backendHost}`;
+
+  return { branchName, frontendHost, backendHost, backendAddress };
 };
